c77b16f2b7606ca1cedfbcc8563b5955dc86f47d
Gravitar64 <gravitar@web.de>
2024-12-03 18:58:36 +0100

    leichte Optimierungen

--- day03.py
+++ day03.py
@@ -8,17 +8,15 @@ def load(file):
 
 def solve(p):
   part1 = part2 = 0
-  
   enabled = True
-  for dont, do, m1, m2 in re.findall(r'(don\'t\(\))|(do\(\))|mul\((\d+),(\d+)\)', p):
-    if dont or do:
-      enabled = do
-      continue
-    e = int(m1) * int(m2)
-    part1 += e
-    if not enabled: continue
-    part2 += e
-
+  
+  for dont, do, x, y in re.findall(r'(don\'t\(\))|(do\(\))|mul\((\d+),(\d+)\)', p):
+    if dont or do: 
+      enabled = bool(do)
+    else:
+      part1 += (e := int(x) * int(y))
+      part2 += e * enabled
+  
   return part1, part2
 
 
