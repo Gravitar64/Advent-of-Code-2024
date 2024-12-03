6a0e97b9abb85725a2ed951f8f3a6358c6b4882d
Gravitar64 <gravitar@web.de>
2024-12-03 11:58:17 +0100

    match aufgebrochen

--- day03.py
+++ day03.py
@@ -4,22 +4,20 @@ import time, re
 def load(file):
   with open(file) as f:
     return f.read()
-  
+
 
 def solve(p):
   part1 = part2 = 0
   
   enabled = True
-  for match in re.findall(r'(don\'t\(\))|(do\(\))|mul\((\d+),(\d+)\)',p):
-    if match[0]:
-      enabled = False
-    elif match[1]:
-      enabled = True
-    else:
-      e = int(match[2]) * int(match[3])
-      part1 += e
-      if not enabled: continue
-      part2 += e  
+  for dont, do, m1, m2 in re.findall(r'(don\'t\(\))|(do\(\))|mul\((\d+),(\d+)\)', p):
+    if dont or do:
+      enabled = do
+      continue
+    e = int(m1) * int(m2)
+    part1 += e
+    if not enabled: continue
+    part2 += e
 
   return part1, part2
 
