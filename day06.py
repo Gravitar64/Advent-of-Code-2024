3c357bd5ebe2d7a0fc00bc9247acbac8a799e42f
Gravitar64 <gravitar@web.de>
2024-12-06 08:16:30 +0100

    V0.2

--- day06.py
+++ day06.py
@@ -6,17 +6,18 @@ def load(file):
     return {(x, y): char for y, line in enumerate(f.read().split('\n')) for x, char in enumerate(line)}
 
 
-def run_guard_run(p, guard_pos, guard_dir, dirs):
+def run_guard_run(p, guard_pos, guard_dir):
   distinct_pos = set()
   counter = 0
   while True:
-    dx, dy = dirs[guard_dir][0]
-    x, y = guard_pos
+    (x, y), (dx, dy) = guard_pos, guard_dir
     nx, ny = x + dx, y + dy
-    if (nx, ny) not in p:
-      return distinct_pos
+
+    if (nx, ny) not in p: return distinct_pos
+    if counter > 1000: return 'loop'
+
     if p[nx, ny] == '#':
-      guard_dir = dirs[guard_dir][1]
+      guard_dir = -dy, dx
     else:
       guard_pos = nx, ny
       if (nx, ny) in distinct_pos:
@@ -24,22 +25,19 @@ def run_guard_run(p, guard_pos, guard_dir, dirs):
       else:
         counter = 0
       distinct_pos.add((nx, ny))
-    if counter > 1000: return 'loop'
 
 
 def solve(p):
   part1 = part2 = 0
-  dirs = {'^': ((0, -1), '>'), '>': ((1, 0), 'v'), '<': ((-1, 0), '^'), 'v': ((0, 1), '<')}
   guard_pos = [pos for pos, char in p.items() if char == '^'][0]
-  guard_dir = p[guard_pos]
-  
-  distinct_pos = run_guard_run(p, guard_pos, guard_dir, dirs) | {guard_pos}
-  part1 = len(distinct_pos)
-  
-  for x,y in distinct_pos:
+  guard_dir = (0, -1)
+
+  distinct_pos = run_guard_run(p, guard_pos, guard_dir)
+  part1 = len(distinct_pos) + 1
+
+  for x, y in distinct_pos:
     p[x, y] = '#'
-    e = run_guard_run(p, guard_pos, guard_dir, dirs)
-    if e == 'loop': part2 += 1
+    if run_guard_run(p, guard_pos, guard_dir) == 'loop': part2 += 1
     p[x, y] = '.'
 
   return part1, part2
