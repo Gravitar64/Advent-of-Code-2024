d8cdc3440b2afe7bb3bf4dab3174bcbcd262a88e
Gravitar64 <gravitar@web.de>
2025-01-04 14:15:49 +0100

    del list at list(map...

--- day22.py
+++ day22.py
@@ -3,7 +3,7 @@ import time, re, itertools as itt, collections as coll
 
 def load(file):
   with open(file) as f:
-    return list(map(int, re.findall('\d+', f.read())))
+    return map(int, re.findall('\d+', f.read()))
 
 
 def mix_prune(s):
